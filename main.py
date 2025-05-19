82683885fedf881026b836ac548b6166dc6582d8
Jakub Robaszek <112584909+jakubrobaszek@users.noreply.github.com>
2025-05-19 22:32:02 +0200

    Added main.py file that handles the entire project

---
+++ main.py
@@ -0,0 +1,20 @@
+try:
+	from flywithdsc import config
+	print("INFO: Config was loaded successfuly!")
+except ImportError as e:
+	print("ERROR: {e}")
+	import sys
+	sys.exit(1)
+
+def run_application():
+	"""Main function responsible for the entire program"""
+	print("Loading FlyWithDsc...")
+	print(f"The loaded discord token is: {'loaded correctly' if config.DISCORD_BOT_TOKEN else 'NO TOKEN!!!'}")
+
+	# Check if the discord token is not loaded and exit
+	if not config.DISCORD_BOT_TOKEN:
+		print("Discord token was not properly loaded check your .env file")
+		return
+
+if __name__ == "__main__":
+	run_application()
