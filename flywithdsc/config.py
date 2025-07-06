f614b8810ddd41e87e5fdf4cf2baf19b89121e56
Jakub Robaszek <112584909+jakubrobaszek@users.noreply.github.com>
2025-07-06 14:47:59 +0200

    Added more comments and made the change to use pathlib for pathing

--- flywithdsc/config.py
+++ flywithdsc/config.py
@@ -1,17 +1,17 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-ABSOLUTE_PATH_TO_CONFIG_FILE = os.path.abspath(__file__)
-CONFIG_DIR = os.path.dirname(ABSOLUTE_PATH_TO_CONFIG_FILE)
-BASE_DIR = os.path.dirname(CONFIG_DIR)
-DOTENV_PATH = os.path.join(BASE_DIR, '.env')
+# Path to the '.env' file
+DOTENV_PATH = Path(__file__).resolve().parent.parent / '.env'
 
 # Checking if path exists
-if os.path.exists(DOTENV_PATH):
-	load_dotenv(dotenv_path=DOTENV_PATH, verbose=True, override=True)
+if DOTENV_PATH.exists():
+	# (override - overrides the .env k-v pairs even if they exist)
+	load_dotenv(dotenv_path=DOTENV_PATH, override=True)
 	print(f"INFO: Loaded .env file from: {DOTENV_PATH}")
 else:
-	print(f"WARNING: Create .env file in {DOTENV_PATH} using template")
+	print(f"WARNING: Create .env file in {DOTENV_PATH} using '.env_example' template")
 
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL", "flights_data.sqlite3")
