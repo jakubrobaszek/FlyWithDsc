202bfcf8f8dc11a4b4d09fc782c5ad96cbc9fe82
Jakub Robaszek <112584909+jakubrobaszek@users.noreply.github.com>
2025-07-06 15:29:35 +0200

    Changed handling of config loading

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-try:
-	# Trying to import my main package
-	from flywithdsc import config
-	print("INFO: Config was loaded successfuly!")
-except ImportError as e:
-	print("ERROR: {e}")
-	import sys
-	sys.exit(1)
+def load_config():
+	try:
+		# Trying to import my main package
+		from flywithdsc import config
+		print("INFO: Config was loaded successfuly!")
+	except ImportError as e:
+		print("ERROR: {e}")
+		import sys
+		sys.exit(1)
 
 def run_application():
 	"""Main function responsible for the entire program"""
 	print("Loading FlyWithDsc...")
+	load_config()
 	print(f"The loaded discord token is: {'loaded correctly' if config.DISCORD_BOT_TOKEN else 'NO TOKEN!'}")
 
 	# Check if the discord token is not loaded and exit
