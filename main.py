51595d47f52231fb545bed0730c804bb724b68a3
Jakub Robaszek <112584909+jakubrobaszek@users.noreply.github.com>
2025-07-06 14:50:19 +0200

    Small text and comments changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 try:
+	# Trying to import my main package
 	from flywithdsc import config
 	print("INFO: Config was loaded successfuly!")
 except ImportError as e:
@@ -9,12 +10,13 @@ except ImportError as e:
 def run_application():
 	"""Main function responsible for the entire program"""
 	print("Loading FlyWithDsc...")
-	print(f"The loaded discord token is: {'loaded correctly' if config.DISCORD_BOT_TOKEN else 'NO TOKEN!!!'}")
+	print(f"The loaded discord token is: {'loaded correctly' if config.DISCORD_BOT_TOKEN else 'NO TOKEN!'}")
 
 	# Check if the discord token is not loaded and exit
 	if not config.DISCORD_BOT_TOKEN:
 		print("Discord token was not properly loaded check your .env file")
 		return
 
+# Checks if the application was launched directly and was not imported as module
 if __name__ == "__main__":
 	run_application()
